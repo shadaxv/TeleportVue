4409532e729f7cda61d31327dcf60497ac8520f4
shadaxv <aleksandern1116@gmail.com>
2018-04-24 11:11:30 +0200

    Added Teleport model /w controller and migration

---
+++ app/Http/Controllers/TeleportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Teleport;
+use Illuminate\Http\Request;
+
+class TeleportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Teleport  $teleport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teleport $teleport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Teleport  $teleport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Teleport $teleport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Teleport  $teleport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Teleport $teleport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Teleport  $teleport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teleport $teleport)
+    {
+        //
+    }
+}

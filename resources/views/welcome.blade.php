4741ff0cadfdc19e460f65083ad3abeca8de3306
shadaxv <aleksandern1116@gmail.com>
2018-04-26 15:11:59 +0200

    Added favicons and pages titles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,22 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Welcome to Teleport App</title>
 
         <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700&amp;subset=latin-ext" rel="stylesheet">
         <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" >
         <link href="{{ asset('css/mdb.css') }}" rel="stylesheet" type="text/css" >
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="{{{ asset('favicon/apple-touch-icon.png') }}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{{ asset('favicon/favicon-32x32.png') }}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{{ asset('favicon/favicon-16x16.png') }}}">
+        <link rel="manifest" href="{{{ asset('favicon/manifest.json') }}}">
+        <link rel="mask-icon" href="{{{ asset('favicon/safari-pinned-tab.svg') }}}" color="#fff">
+        <link rel="shortcut icon" href="{{{ asset('favicon/favicon.ico') }}}">
+        <meta name="msapplication-config" content="{{{ asset('favicon/browserconfig.xml') }}}">
+        <meta name="theme-color" content="#222">
+        <meta name="msapplication-navbutton-color" content="#222">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#222">
 
         <!-- Styles -->
         <style>
